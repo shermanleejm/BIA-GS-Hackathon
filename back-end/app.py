696d75cbf8240e36d06096e410507843ced16374
Sherman Lee <leesherman@live.com.sg>
2020-08-31 23:34:38 +0800

    basic flask app

---
+++ back-end/app.py
@@ -0,0 +1,53 @@
+import json
+import os
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+# import pandas as pd
+# import time
+# import requests
+# import boto3
+# import atexit
+# from apscheduler.schedulers.background import BackgroundScheduler
+
+app = Flask(__name__)
+CORS(app)
+
+# s3 = boto3.client("s3")
+
+def getJSON(filename):
+    """retrieve a file from the s3 bucket 
+
+    :param name: name of the file to be retrieved
+    :type name: str
+    :return: the JSON file matching the name passed in
+    :rtype: JSON file
+    """   
+    s3_obj = s3.get_object(Bucket="accredify-social-analytics", Key=filename)
+    return s3_obj["Body"].read().decode("utf-8")
+
+
+def putJSON(obj, filename):
+    """upload a file onto the s3 bucket
+
+    :param obj: a JSON file that contains relevant data
+    :type obj: JSON file
+    :param filename: a name that describes the content of the file
+    :type filename: string
+    """   
+    s3.put_object(
+        Body=json.dumps(obj, indent=2),
+        Bucket="accredify-social-analytics",
+        Key=filename,
+    )
+
+@app.route("/")
+def home():
+    """the home page
+
+    :return: the greeting message for the homepage
+    :rtype: str
+    """    
+    return "Welcome to BIA"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
