01ab9eca5c18cd1b41f66bfb5ad25b5af8dcab24
Ong Jun Xiang <ongjx9@hotmail.com>
2020-09-04 12:11:22 +0800

    Feature Update: Modify handling of questionnaires

--- front-end/src/components/questionnaire/containers/OccupationQuestionContainer.jsx
+++ front-end/src/components/questionnaire/containers/OccupationQuestionContainer.jsx
@@ -1,40 +1,40 @@
 import React, { useState } from 'react';
 import classnames from 'classnames';
 import styles from './QuestionContainer.module.scss'
-import SpendingQuestionContainer from './SpendingQuestionContainer'
 
-const OccupationQuestionContainer = () => {
-    const [display, setDisplay] = useState(true);
-
+const OccupationQuestionContainer = (props) => {
+    const [choice, setChoice] = useState();
+    const handleClick = (occupation) => {
+        setChoice(occupation)
+        props.setOccupation(occupation)
+    }
     return (
-        display ?
-        <form className={classnames('pt-5',styles["form"])}>
+        <form className={classnames('pt-5',styles["form"])} hidden={props.currentQuestion !== 1}>
             <h2 className='mb-4'>What is your occupation?</h2>
             <div className={classnames(styles["inputGroup"])}>
-                <input id="doctor" name="radio" type="radio"/>
+                <input id="doctor" name="radio" type="radio" onClick={()=>{handleClick('Doctor')}}/>
                 <label for="doctor">Doctor</label>
             </div>
             <div className={styles["inputGroup"]}>
-                <input id="student" name="radio" type="radio"/>
+                <input id="student" name="radio" type="radio" onClick={()=>{handleClick('Student')}}/>
                 <label for="student">Student</label>
             </div>
             <div className={styles["inputGroup"]}>
-                <input id="freshgrad" name="radio" type="radio"/>
+                <input id="freshgrad" name="radio" type="radio" onClick={()=>{handleClick('Fresh Graduate')}}/>
                 <label for="freshgrad">Fresh Graduate</label>
             </div>
             <div className={styles["inputGroup"]}>
-                <input id="retired" name="radio" type="radio"/>
+                <input id="retired" name="radio" type="radio" onClick={()=>{handleClick('Retired')}}/>
                 <label for="retired">Retired</label>
             </div>
             <div className={styles["inputGroup"]}>
-                <input id="others" name="radio" type="radio"/>
+                <input id="others" name="radio" type="radio" onClick={()=>{handleClick('Others')}}/>
                 <label for="others">Others</label>
             </div>
 
 
-            <button className='btn btn-lg block px-5 btn-dark' onClick={()=>{setDisplay(false)}}>Next</button>
-        </form> :
-        <SpendingQuestionContainer/>
+            {/* <button className='btn btn-lg block px-5 btn-dark' onClick={()=>{setDisplay(false)}}>Next</button> */}
+        </form>
     )
 
 }
--- front-end/src/components/questionnaire/containers/RiskQuestionContainer.jsx
+++ front-end/src/components/questionnaire/containers/RiskQuestionContainer.jsx
@@ -1,44 +1,34 @@
 import React, { useState } from 'react';
 import classnames from 'classnames';
 import styles from './QuestionContainer.module.scss'
-import LoadingOverlay from 'react-loading-overlay';
-import RiskProfileContainer from '../../riskProfile/containers/RiskProfileContainer';
 
-const RiskQuestionContainer = () => {
+const RiskQuestionContainer = (props) => {
     const [isActive, setIsActive] = useState(false);
-    const [display, setDisplay] = useState(true);
+    const [choice, setChoice] = useState();
 
-    const handleClick = () => {
+    const handleClick = (risk) => {
         setIsActive(true)
-        setDisplay(false);
+        setChoice(risk)
+        props.setRisk(risk)
     }
     return (
-        display ?
-        <LoadingOverlay
-            active={isActive}
-            spinner
-            text='Generating your risk portfolio...'
-            fadeSpeed={500}
-            >
-                <form className={classnames('pt-5',styles["form"])}>
+
+                <form className={classnames('pt-5',styles["form"])} hidden={props.currentQuestion !== 3}>
                     <h2 className='mb-4'>What is your risk appetite?</h2>
                     <div className={classnames(styles["inputGroup"])}>
-                        <input id="high" name="radio" type="radio"/>
+                        <input id="high" name="radio" type="radio" onClick={()=>{handleClick('High')}}/>
                         <label for="high">High</label>
                     </div>
                     <div className={styles["inputGroup"]}>
-                        <input id="medium" name="radio" type="radio"/>
+                        <input id="medium" name="radio" type="radio" onClick={()=>{handleClick('Medium')}}/>
                         <label for="medium">Medium</label>
                     </div>
                     <div className={styles["inputGroup"]}>
-                        <input id="low" name="radio" type="radio"/>
+                        <input id="low" name="radio" type="radio" onClick={()=>{handleClick('Low')}}/>
                         <label for="low">Low</label>
                     </div>
 
-                    <button className='btn btn-lg block px-5 btn-dark' onClick={handleClick}>Generate my Risk Portfolio!</button>
                 </form>
-            </LoadingOverlay>
-            : <RiskProfileContainer />
         )
 
 }
