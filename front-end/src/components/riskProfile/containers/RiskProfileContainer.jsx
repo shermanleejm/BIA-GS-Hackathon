f4a4a6d1e8a86d4b49b68d8b1356b3b2a9b0415c
Ong Jun Xiang <ongjx9@hotmail.com>
2020-09-04 11:09:43 +0800

    Added container to div

--- front-end/src/components/riskProfile/containers/RiskProfileContainer.jsx
+++ front-end/src/components/riskProfile/containers/RiskProfileContainer.jsx
@@ -3,44 +3,44 @@ import React from 'react';
 const RiskProfileContainer = () => {
 
     return (
-
-        <div className="card">
-            <div className="card-body">
-                <h3 className="card-title">You are a <span className='badge badge-secondary'>LOW RISK</span> profile!</h3>
-                <p className="card-text">These are your potential focus areas:</p>
-                <div className='row text-left mb-4'>
-                    <div className='col-md-4 col-sm-12'>
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Futures</h5>
-                                <p className="card-text"><small>A legal agreement to buy or sell a particular commodity asset, or security at a predetermined price at a specified time in the future.</small></p>
-                                <a href="#" className="btn btn-primary">Learn More</a>
+        <div className='container'>
+            <div className="card">
+                <div className="card-body">
+                    <h3 className="card-title">You are a <span className='badge badge-secondary'>LOW RISK</span> profile!</h3>
+                    <p className="card-text">These are your potential focus areas:</p>
+                    <div className='row text-left mb-4'>
+                        <div className='col-md-4 col-sm-12'>
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Futures</h5>
+                                    <p className="card-text"><small>A legal agreement to buy or sell a particular commodity asset, or security at a predetermined price at a specified time in the future.</small></p>
+                                    <a href="#" className="btn btn-primary">Learn More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-md-4 col-sm-12'>
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">ETFs</h5>
+                                    <p className="card-text"><small>A type of security that involves a collection of securities—such as stocks—that often tracks an underlying index</small></p>
+                                    <a href="#" className="btn btn-primary">Learn More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-md-4 col-sm-12'>
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">REITs</h5>
+                                    <p className="card-text"><small>Allows individual investors to earn dividends from real estate investments—without having to buy, manage, or finance any properties themselves.</small></p>
+                                    <a href="#" className="btn btn-primary">Learn More</a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className='col-md-4 col-sm-12'>
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">ETFs</h5>
-                                <p className="card-text"><small>A type of security that involves a collection of securities—such as stocks—that often tracks an underlying index</small></p>
-                                <a href="#" className="btn btn-primary">Learn More</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='col-md-4 col-sm-12'>
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">REITs</h5>
-                                <p className="card-text"><small>Allows individual investors to earn dividends from real estate investments—without having to buy, manage, or finance any properties themselves.</small></p>
-                                <a href="#" className="btn btn-primary">Learn More</a>
-                            </div>
-                        </div>
-                    </div>
+                    <a href="#" className="btn btn-primary">CONTINUE</a>
                 </div>
-                <a href="#" className="btn btn-primary">CONTINUE</a>
             </div>
         </div>
-
     );
 }
 
